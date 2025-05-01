228eb33610d139adc39a310632b0438f79fb9ebc
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-05-01 14:24:52 +0100

    Make overlays more configurable

---
+++ pico_overlay/include/pico/overlay.h
@@ -0,0 +1,18 @@
+
+#ifndef _OVERLAYS_H
+#define _OVERLAYS_H
+
+#include "pico/binary_info.h"
+#include "pico/sha256.h"
+
+#include "all_overlays.h"
+
+#define __call_overlay_func(name, ...) if (overlay_load(__load_start_overlay_##name, __load_stop_overlay_##name, name##_hash)) { name##_internal(__VA_ARGS__); } else { printf("overlay failed to load\n"); }
+#define __overlay_func(name, overlay) __noinline __attribute__((section("." __STRING(overlay) "." __STRING(name)))) name##_internal
+
+extern uint8_t __overlays_start[];
+extern uint8_t __overlays_end[];
+
+bool overlay_load(uint8_t* start, uint8_t* stop, char* hash);
+
+#endif
---
+++ pico_overlay/overlay.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include "pico/stdlib.h"
+#include "pico/sha256.h"
+#include "pico/overlay.h"
+
+
+bool overlay_load(uint8_t* start, uint8_t* stop, char* hash) {
+    static uint8_t* current_start = NULL;
+    if (current_start == start) {
+        printf("overlay already loaded\n");
+        return true;
+    }
+    current_start = start;
+    printf("loading overlay from %p into %p\n", start, __overlays_start);
+    memcpy(__overlays_start, start, stop - start);
+    memset(__overlays_start + (stop - start), 0, __overlays_end - __overlays_start - (stop - start));
+
+    printf("checking hash\n");
+    pico_sha256_state_t state;
+    int rc = pico_sha256_start_blocking(&state, SHA256_BIG_ENDIAN, true); // using some DMA system resources
+    hard_assert(rc == PICO_OK);
+    pico_sha256_update_blocking(&state, (const uint8_t*)__overlays_start, __overlays_end - __overlays_start);
+
+    // Get the result of the sha256 calculation
+    sha256_result_t result;
+    pico_sha256_finish(&state, &result);
+
+    // Because the hash is stored as a string, we need to convert it to a byte array
+    char *pos = hash;
+    sha256_result_t expected;
+    for (size_t count = 0; count < sizeof(expected.bytes); count++) {
+        sscanf(pos, "%2hhx", &expected.bytes[count]);
+        pos += 2;
+    }
+
+    if(memcmp(&expected, &result, SHA256_RESULT_BYTES) == 0) {
+        printf("hash is correct\n");
+        return true;
+    } else {
+        printf("hash is incorrect - expected:\n");
+        for(int i = 0; i < SHA256_RESULT_BYTES; i++) {
+            printf("0x%02x,", result.bytes[i]);
+        }
+        printf("\n");
+        memset(__overlays_start, 0, __overlays_end - __overlays_start);
+        return false;
+    }
+}
